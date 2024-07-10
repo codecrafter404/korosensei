39ef6b0cf48365985eb8c9b308dcfe441d566430
Codecrafter_404 <codecrafter404@github.com>
2024-07-10 18:16:28 +0200

    finished github integration

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,19 @@
-use std::ops::Deref;
-
-use color_eyre::{
-    eyre::{eyre, OptionExt},
-    owo_colors::OwoColorize,
-};
+use color_eyre::eyre::{eyre, OptionExt};
 use futures::StreamExt;
-use graph_rs_sdk::{http::HttpResponseExt, Graph, GraphClient, ODataQuery};
+use graph_rs_sdk::{http::HttpResponseExt, GraphClient, ODataQuery};
 use serde::Deserialize;
 use worker::*;
 
 #[event(scheduled)]
-async fn scheduled(event: worker::ScheduledEvent, env: Env, context: ScheduleContext) {}
-
+async fn scheduled(_event: worker::ScheduledEvent, env: Env, _context: ScheduleContext) {
+    match handle_scheduled_event(env).await {
+        Ok(_) => {}
+        Err(why) => {
+            console_error!("Error while handling scheduled event: {:#?}", why);
+        }
+    }
+}
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct OneDriveChildren {
@@ -19,17 +21,19 @@ struct OneDriveChildren {
     file: Option<OneDriveFile>,
     folder: Option<OneDriveFolder>,
 }
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct OneDriveFolder {
     child_count: i32,
 }
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct OneDriveFile {
     mime_type: Option<String>,
 }
-
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 struct GithubTreeNode {
     path: String,
@@ -40,49 +44,56 @@ struct GithubTreeNode {
     url: String,
     size: i32,
 }
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 struct GithubTreeResponse {
     sha: String,
     url: String,
     tree: Vec<GithubTreeNode>,
 }
-async fn handle_scheduled_evnet(env: Env) -> color_eyre::Result<()> {
+async fn get_kv_key(env: &Env, key: &str) -> color_eyre::Result<String> {
     let kv = env.kv("kv")?;
+    let res = kv
+        .get(key)
+        .text()
+        .await
+        .map_err(|e| eyre!("{:?}", e))?
+        .ok_or_eyre(format!("expected {} to be set", key))?;
+
+    Ok(res)
+}
+async fn handle_scheduled_event(env: Env) -> color_eyre::Result<()> {
     // GitHub
-    let github_api_key = kv
-        .get("github_api_key")
-        .text()
-        .await
-        .map_err(|e| eyre!("{:?}", e))?
-        .ok_or_eyre("expected github_api_key to be set")?;
-    let github_repo = kv
-        .get("github_repo")
-        .text()
-        .await
-        .map_err(|e| eyre!("{:?}", e))?
-        .ok_or_eyre("expected github_repo to be set")?;
+    let github_repo_path = get_kv_key(&env, "github_repo_path").await?;
+    let github_repo_path = github_repo_path
+        .strip_prefix("/")
+        .unwrap_or(&github_repo_path)
+        .to_owned();
+
+    let github_repo_path = github_repo_path
+        .strip_suffix("/")
+        .unwrap_or(&github_repo_path)
+        .to_owned();
+
+    let github_api_key = get_kv_key(&env, "github_api_key").await?;
+    let github_repo = get_kv_key(&env, "github_repo").await?;
     let github_repo = github_repo.split("/").collect::<Vec<_>>();
     if github_repo.len() != 2 {
         return Err(eyre!("Expected github_repo in format owner/repository"));
     }
-    let github_repo_branch = kv
-        .get("github_repo_branch")
-        .text()
-        .await
-        .map_err(|e| eyre!("{:?}", e))?
-        .ok_or_eyre("expected github_repo_branch to be set")?;
+    let github_repo_branch = get_kv_key(&env, "github_repo_branch").await?;
 
-    let client = octocrab::OctocrabBuilder::new()
+    let github_client = octocrab::OctocrabBuilder::new()
         .personal_token(github_api_key)
         .build()?;
-    let mut current_branch_list = client
+    let mut current_branch_list = github_client
         .repos(github_repo[0], github_repo[1])
         .list_branches()
         .per_page(100)
         .send()
         .await?;
     let mut branches = current_branch_list.take_items();
-    while let Ok(Some(mut new_page)) = client.get_page(&current_branch_list.next).await {
+    while let Ok(Some(mut new_page)) = github_client.get_page(&current_branch_list.next).await {
         branches.extend(new_page.take_items());
         current_branch_list = new_page;
     }
@@ -94,38 +105,55 @@ async fn handle_scheduled_evnet(env: Env) -> color_eyre::Result<()> {
         return Err(eyre!("Branch {} doesn't exist", github_repo_branch));
     }
 
-    let res: GithubTreeResponse = client
+    let res: GithubTreeResponse = github_client
         .get(
             format!(
                 "/repos/{}/{}/git/trees/{}",
                 github_repo[0], github_repo[1], github_repo_branch
             ),
-            Some(""),
+            Some("?recursive=1"),
         )
         .await?;
 
+    let github_files = res
+        .tree
+        .into_iter()
+        .filter(|x| x._type == "blob" && x.path.starts_with(&github_repo_path))
+        .map(|x| {
+            x.path
+                .strip_prefix(&format!("{}/", github_repo_path))
+                .ok_or_eyre(format!(
+                    "Expect {} to have prefix {}",
+                    x.path,
+                    format!("{}/", github_repo_path)
+                ))
+                .map(|x| x.to_owned())
+        })
+        .collect::<color_eyre::Result<Vec<String>>>()?;
+
     // OneDrive
     let token = get_access_token(env.clone()).await?;
-    let client = GraphClient::new(token);
+    let graph_client = GraphClient::new(token);
 
-    let path = env
-        .kv("kv")?
-        .get("onedrive_path")
-        .text()
-        .await
-        .map_err(|e| color_eyre::eyre::eyre!("{:?}", e.to_string()))?
-        .ok_or_eyre("Expected onedrive_path to be set")?;
+    let onedrive_path = get_kv_key(&env, "onedrive_path").await?;
 
-    let path = path.strip_suffix("/").map(|x| x.to_owned()).unwrap_or(path);
+    let mut onedrive_path = onedrive_path
+        .strip_suffix("/")
+        .map(|x| x.to_owned())
+        .unwrap_or(onedrive_path);
+    if !onedrive_path.starts_with("/") {
+        onedrive_path = format!("/{}", onedrive_path);
+    }
 
-    let mut children = client
+    let mut children = graph_client
         .me()
         .drive()
-        .item_by_path(format!(":/{}:", path))
+        .item_by_path(format!(":/{}:", onedrive_path))
         .list_children()
         .select(&["name", "folder", "file"])
         .paging()
         .stream::<OneDriveChildren>()?;
+    let mut files_to_sync = Vec::new();
     while let Some(result) = children.next().await {
         let res = result?;
         if !res.status().is_success() {
@@ -136,28 +164,49 @@ async fn handle_scheduled_evnet(env: Env) -> color_eyre::Result<()> {
             ));
         }
         let res = res.into_body()?;
+        if res.folder.is_some() {
+            continue;
+        }
+        let name = format!("{}.link", res.name);
+        if !github_files.contains(&name) {
+            files_to_sync.push(name);
+        }
     }
-    unimplemented!()
+
+    // syncing files
+    for file in files_to_sync {
+        let github_path = format!("{}/{}", github_repo_path, file);
+        let onedrive_file = file.strip_suffix(".link").expect("Infallible").to_owned();
+        let onedrive_path = format!("{}/{}", onedrive_path, onedrive_file);
+        match github_client
+            .repos(github_repo[0], github_repo[1])
+            .create_file(
+                github_path.clone(),
+                format!("Synced file onedrive:{} -> {}", onedrive_path, github_path),
+                format!("onedrive:{}", onedrive_path),
+            )
+            .send()
+            .await
+        {
+            Ok(_) => {
+                console_log!("Synced file onedrive:{} -> {}", onedrive_path, github_path);
+            }
+            Err(why) => {
+                console_error!(
+                    "Error while syncing file (onedrive:{} -> {}): {:?}",
+                    onedrive_path,
+                    github_path,
+                    why
+                );
+            }
+        }
+    }
+    Ok(())
 }
 
-async fn get_access_token(env: Env) -> worker::Result<String> {
-    let kv = env.kv("kv")?;
-    let url = match kv.get("access_token_url").text().await? {
-        Some(x) => x,
-        None => {
-            return Err(worker::Error::RustError(
-                "expected access_token_url to be set".into(),
-            ))
-        }
-    };
-    let authorization_header = match kv.get("access_token_authorization").text().await? {
-        Some(x) => x,
-        None => {
-            return Err(worker::Error::RustError(
-                "expected access_token_authorization to be set".into(),
-            ))
-        }
-    };
+async fn get_access_token(env: Env) -> color_eyre::Result<String> {
+    let url = get_kv_key(&env, "onedrive_access_token_url").await?;
+    let authorization_header = get_kv_key(&env, "onedrive_access_token_authorization").await?;
     let client = reqwest::Client::new();
     let res = client
         .get(url)
@@ -166,17 +215,15 @@ async fn get_access_token(env: Env) -> worker::Result<String> {
         .await
         .map_err(|e| worker::Error::RustError(format!("get_access_token_res: {:?}", e).into()))?;
     if !res.status().is_success() {
-        return Err(worker::Error::RustError(format!(
+        return Err(eyre!(
             "get_access_token_res; access_token_url returned {}: {:?}",
             res.status().as_u16(),
             res.text().await
-        )));
+        ));
     }
-    let text = res.text().await.map_err(|e| {
-        worker::Error::RustError(format!(
-            "get_access_token: Failed to read response text: {:?}",
-            e
-        ))
-    })?;
+    let text = res
+        .text()
+        .await
+        .map_err(|e| eyre!("get_access_token: Failed to read response text: {:?}", e))?;
     Ok(text)
 }
